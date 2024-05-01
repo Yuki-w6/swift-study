c0645142d6b2b7f1c2bb8253a6d43e729d1980d2
Yuki Mayama <m1610600@gmail.com>
2024-05-01 18:40:22 +0900

    関数化

--- study/study/ContentView.swift
+++ study/study/ContentView.swift
@@ -2,27 +2,43 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        ScrollViewContainer()
+    }
+}
+
+struct ScrollViewContainer: View {
+    var body: some View {
         ScrollView {
             GeometryReader { geometry in
                 VStack(spacing: 0) {
                     ForEach(0..<24, id: \.self) { hour in
-                        HStack {
-                            Text("\(hour):00")
-                                .frame(width: geometry.size.width / 6, height: 60, alignment: .leading)
-                                .foregroundColor(.black)
-                                .padding(.leading, 10)
-                            
-                            Divider()
-                                .background(Color.black) // ここで線の色を指定します（黒または白）
-                            
-                            Spacer() // 残りのスペースを埋めるためのスペーサー
-                        }
-                        .frame(maxWidth: .infinity, minHeight: 60)
-                        .background(Color.white) // 背景色が必要な場合はここで設定
+                        HourView(width: geometry.size.width / 6, hour: hour)
+                        Divider()
                     }
                 }
             }
         }
+    }
+}
+
+struct HourView: View {
+    let width: CGFloat
+    let hour: Int
+    
+    var body: some View {
+        HStack(alignment: .center, spacing: 0) {
+            Text("\(hour):00")
+                .frame(width: width, height: 60, alignment: .leading)
+                .padding(.leading, 10)
+                .background(Color.white)
+            
+            Divider() // Dividerをテキストの横に配置
+                .frame(height: 60) // Dividerの高さをテキストの高さに合わせる
+            
+            Spacer() // 残りのスペースを埋めるためのスペーサー
+        }
+        .frame(maxWidth: .infinity, minHeight: 60)
+        .background(Color.white)
     }
 }
 
